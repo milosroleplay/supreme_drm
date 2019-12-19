c29161878e2232ce18c3021530751d0754aa673c
milosroleplay <47277353+milosroleplay@users.noreply.github.com>
2019-12-19 18:11:43 +0500

    Create drm_ch.lua

---
+++ drm_ch.lua
@@ -0,0 +1,6 @@
+drm_d = {}
+drm_d.Version = "1.0.0"
+drm_d.VersionType = "Stable"
+drm_d.LogCenter = ""
+
+drm_d.Agenda = "Version 1.0.0 (Stable) - Initial Version. Starting work for community that needs a drm for their addon."
