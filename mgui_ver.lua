30b24004a9ef6e62fe6771f5ac763251cba0e9e4
milosroleplay <47277353+milosroleplay@users.noreply.github.com>
2020-01-29 14:40:54 +0500

    Update mgui_ver.lua

--- mgui_ver.lua
+++ mgui_ver.lua
@@ -1,7 +1,11 @@
 mgui = {}
 mgui.newver = "1.0.0"
 mgui.newvertype = "Stable"
-mgui.updatelog = "Version 1.0.0:\n + Added update checker\n + Added auto-update\n + Lib created!\n"
+mgui.updatelog = "Version 1.0.0:\nAdded update checker\nAdded auto-update\nLib created!\n"
 function mgui.webPrint( color, ... )
 	return MsgC(Grey,"[",LightGreen,"AutoUpdate",Grey,"] ",color,...,color_white,"\n")
+end
+function mgui.UpdatedScript()
+	return 	MsgC(Grey,"[",LightGreen,"AutoUpdate",Grey,"] ",Blue,"Starting update...",color_white,"\n")
+	Cool = Color(100,100,255)
 end
