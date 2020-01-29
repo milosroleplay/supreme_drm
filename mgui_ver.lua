abca30fa48687e0a1d9ff86f4b4f5ae205965416
milosroleplay <47277353+milosroleplay@users.noreply.github.com>
2020-01-29 17:39:58 +0500

    Update mgui_ver.lua

--- mgui_ver.lua
+++ mgui_ver.lua
@@ -5,6 +5,3 @@ mgui.updatelog = "Version 1.0.0:\nAdded update checker\nAdded auto-update\nLib c
 function mgui.webPrint( color, ... )
 	return MsgC(Color(170,170,170),"[",Color(100,255,100),"Update Checker",Color(170,170,170),"] ",color,...,color_white,"\n")
 end
-function mgui.chatPrint( color, ... )
-	return chat.AddText(Color(170,170,170),"[",Color(255,255,100),"MGUI",Color(170,170,170),"] ",color,...,color_white,"\n")
-end
