600b146b78fb2eee4d133ad2ac9eaf949501cc08
milosroleplay <47277353+milosroleplay@users.noreply.github.com>
2020-02-02 20:32:53 +0500

    Update mgui_ver.lua

--- mgui_ver.lua
+++ mgui_ver.lua
@@ -4,7 +4,7 @@ site = {}
 mgui.newver = "1.0.0"
 mgui.newvertype = "Stable"
 mgui.updatelog = "Version 1.0.0:\nAdded update checker\nAdded auto-update\nLib created!\n"
-function site.webPrint( color, ... )
+function webPrint( color, ... )
 	return MsgC(Color(170,170,170),"[",Color(100,255,100),"Update Checker",Color(170,170,170),"] ",color,...,color_white,"\n")
 end
 gl_mltr.newversion = "1.0.1"
