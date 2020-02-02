7ba53e2b1d3f66f26195312ed6f078e34ab7ed6d
milosroleplay <47277353+milosroleplay@users.noreply.github.com>
2020-02-02 21:00:52 +0500

    Update mgui_ver.lua

--- mgui_ver.lua
+++ mgui_ver.lua
@@ -8,8 +8,3 @@ function webPrint( color, ... )
 	return MsgC(Color(170,170,170),"[",Color(100,255,100),"Update Checker",Color(170,170,170),"] ",color,...,color_white,"\n")
 end
 gl_mltr.newversion = "1.0.1"
-surface.CreateFont('font_error_hud', {
-    font = 'Default',
-    size = 20,
-    weight = 600,
-} )
