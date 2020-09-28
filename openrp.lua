421838133829b6b99484abb5b982b93ce3bc18ee
milosroleplay <47277353+milosroleplay@users.noreply.github.com>
2020-09-28 19:03:46 +0500

    Update openrp.lua

--- openrp.lua
+++ openrp.lua
@@ -4,3 +4,8 @@ es.news[1] = {
   title = "Запуск",
   desc = [[Запускаем наш сервер!]],
 }
+es.news[2] = {
+  image = "https://i.imgur.com/Xd2Ueyg.jpg",
+  title = "Запуск",
+  desc = [[Запускаем наш сервер!]],
+}
