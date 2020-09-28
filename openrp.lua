623bc461953c87ee722b5fb11c18644ea1e40b90
milosroleplay <47277353+milosroleplay@users.noreply.github.com>
2020-09-28 19:59:10 +0500

    Update openrp.lua

--- openrp.lua
+++ openrp.lua
@@ -2,7 +2,10 @@ es.news = {}
 es.news["last"] = {
   image = "https://i.imgur.com/Xd2Ueyg.jpg",
   title = "Запуск",
-  desc = [[Последняя новость!\nСервер в стадии ОБТ!\nУспейте получить бонусы!\nКуча новых бонусов!]],
+  desc = [[Последняя новость!
+Сервер в стадии ОБТ!
+Успейте получить бонусы!
+Куча новых бонусов!]],
 }
 es.news["notlast_1"] = {
   image = "https://i.imgur.com/Xd2Ueyg.jpg",
